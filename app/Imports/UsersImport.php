7b7a4569fc4717db5cfe543f2e1a1fbf277789a1
Iftekhar <iftekharul@augnitive.com>
2020-01-29 17:00:17 +0600

    Hot fix Excel import date format issue

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -10,6 +10,7 @@ use Maatwebsite\Excel\Concerns\ToCollection;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
+use DateTime;
 
 class UsersImport implements ToCollection, WithHeadingRow
 {
@@ -25,6 +26,7 @@ class UsersImport implements ToCollection, WithHeadingRow
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
+
 
     public function collection(Collection $rows)
     {
@@ -78,7 +80,7 @@ class UsersImport implements ToCollection, WithHeadingRow
                 'version' => $row['version'],
                 'shift' => isset($row['shift']) ? $row['shift'] : '',
                 'group' => isset($row['group']) ? $row['group'] : '',
-                'birthday' => is_string($row['birthday']) ? Carbon::parse((string)$row['birthday']) : Date::excelToDateTimeObject($row['birthday']),
+                'birthday' => is_string($row['birthday']) ? Carbon::createFromFormat('d/m/Y', $row['birthday']) : Date::excelToDateTimeObject($row['birthday']),
                 'father_name' => $row['father_name'],
                 'father_phone_number' => $row['father_phone_number'],
                 'father_national_id' => $row['father_national_id'],
@@ -134,7 +136,7 @@ class UsersImport implements ToCollection, WithHeadingRow
             && isset($row['gender']) && !empty($row['gender'])
             && isset($row['address']) && !empty($row['address'])
             && isset($row['version']) && !empty($row['version'])
-            && isset($row['birthday']) && !empty($row['birthday']) && !$this->datecheck($row['birthday'])
+            && isset($row['birthday']) && !empty($row['birthday']) && $this->datecheck($row['birthday']) == true
             && isset($row['father_name']) && !empty($row['father_name'])
             && isset($row['father_phone_number']) && !empty($row['father_phone_number'])
             && isset($row['father_occupation']) && !empty($row['father_occupation'])
@@ -147,7 +149,7 @@ class UsersImport implements ToCollection, WithHeadingRow
 
     public function checkDuplicate($student_name, $father_name, $birthday)
     {
-        $birth = is_string($birthday) ? Carbon::parse((string)$birthday) : Date::excelToDateTimeObject($birthday);
+        $birth = is_string($birthday) ? Carbon::createFromFormat('d/m/Y', (string)$birthday) : Date::excelToDateTimeObject($birthday);
         $count = User::join('student_infos', 'users.id', '=', 'student_infos.user_id')
             ->where('users.name', $student_name)
             ->where('student_infos.father_name', $father_name)
@@ -160,10 +162,20 @@ class UsersImport implements ToCollection, WithHeadingRow
     }
     public function datecheck($row)
     {
+        $status = false;
         if (preg_match("/[a-z]/i",$row))
         {
-            return true;
+            $status = true;
         }
-        return false;
+        $status = $this->validateDate($row);
+        return $status;
+    }
+    public function validateDate($date, $format = 'd/m/Y')
+    {
+        if(is_string($date)){
+            $d = DateTime::createFromFormat($format, $date);
+            return $d && $d->format($format) === $date;
+        }
+        return true;
     }
 }
