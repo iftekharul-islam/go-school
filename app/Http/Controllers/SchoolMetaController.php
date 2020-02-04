86daf32e9513dd5d98c54f8691d887664ead24a7
Debashish Roy <debashish@augnitive.com>
2020-02-04 15:31:09 +0600

    invoice generation done

--- app/Http/Controllers/SchoolMetaController.php
+++ app/Http/Controllers/SchoolMetaController.php
@@ -60,9 +60,11 @@ class SchoolMetaController extends Controller
      * @param  \App\SchoolMeta  $schoolMeta
      * @return \Illuminate\Http\Response
      */
-    public function edit(SchoolMeta $schoolMeta)
-    {
-        //
+    public function edit( $id )
+    {   
+        $schoolMeta = SchoolMeta::findOrFail($id);
+        $schools = School::where('is_active', 1)->get();
+        return view('payment.edit-payment-detail', compact('schoolMeta', 'schools'));
     }
 
     /**
@@ -72,9 +74,17 @@ class SchoolMetaController extends Controller
      * @param  \App\SchoolMeta  $schoolMeta
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, SchoolMeta $schoolMeta)
+    public function update(Request $request, $id)
     {
-        //
+        $schoolMeta = SchoolMeta::findOrFail($id);
+        $schoolMeta->school_id = $request->school_id;
+        $schoolMeta->sms_charge = $request->sms_charge;
+        $schoolMeta->per_student_charge = $request->per_student_charge;
+        $schoolMeta->invoice_generation_date = $request->invoice_generation_date;
+        $schoolMeta->email = $request->email;
+        $schoolMeta->save();
+        
+        return back()->with('status', 'Payment info updated');
     }
 
     /**
--- resources/views/payment/edit-payment-detail.blade.php
+++ resources/views/payment/edit-payment-detail.blade.php
@@ -6,14 +6,14 @@
     <div class="breadcrumbs-area">
         <h3>
             <i class="fa fa-usd"></i>
-            Add Payment Information
+            Edit Payment Information
         </h3>
         <ul>
             <li><a href="{{ URL::previous() }}" style="color: #32998f!important;">
                     Back &nbsp;&nbsp;|</a>
                 <a style="margin-left: 8px;" href="{{ url(\Illuminate\Support\Facades\Auth::user()->role.'/home') }}">&nbsp;&nbsp;Home</a>
             </li>
-            <li>Add Payment Information</li>
+            <li>Edit Payment Information</li>
         </ul>
     </div>
     @if (session('status'))
@@ -27,7 +27,7 @@
     @endif
     <div class="card height-auto mb-5">
         <div class="card-body">
-            <form class="new-added-form" method="POST" enctype="multipart/form-data" id="attendanceTime" action="{{ route('store.payment.info') }}">
+            <form class="new-added-form" method="POST" action="{{ route('update.payment.info', ['id' => $schoolMeta->id]) }}">
                 {{ csrf_field() }}
                 <div class="row">
                     <div class="col-md-6">
@@ -37,7 +37,7 @@
                                 <select id="school_id" class="form-control select2" name="school_id" >
                                     <option value="" disabled selected>Select School</option>
                                     @foreach ($schools as $school)
-                                        <option value="{{$school->id}}" @if(old('school_id') == $school->id) selected @endif>{{$school->name}}</option>    
+                                        <option value="{{$school->id}}" @if($schoolMeta->school_id == $school->id) selected @endif>{{$school->name}}</option>    
                                     @endforeach
                                 </select>
                                 @if ($errors->has('school_id'))
@@ -52,7 +52,7 @@
                         <div class="false-padding-bottom-form form-group{{ $errors->has('sms_charge') ? ' has-error' : '' }}">
                             <div class="col-md-12">
                                 <label for="sms_charge" class="control-label false-padding-bottom">SMS Charge(Per SMS)<label class="text-danger">*</label></label>
-                                <input id="sms_charge" type="text" class="form-control" name="sms_charge" value="{{old('sms_charge')}}" placeholder="0.35">
+                                <input id="sms_charge" type="text" class="form-control" name="sms_charge" value="{{$schoolMeta->sms_charge}}" placeholder="0.35">
                                 @if ($errors->has('sms_charge'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('sms_charge') }}</strong>
@@ -65,7 +65,7 @@
                         <div class="false-padding-bottom-form form-group{{ $errors->has('per_student_charge') ? ' has-error' : '' }}">
                             <div class="col-md-12">
                                 <label for="per_student_charge" class="control-label false-padding-bottom">Per Student Charge<label class="text-danger">*</label></label>
-                                <input id="per_student_charge" type="text" class="form-control" name="per_student_charge" value="{{old('per_student_charge')}}" placeholder="5.00">
+                                <input id="per_student_charge" type="text" class="form-control" name="per_student_charge" value="{{$schoolMeta->per_student_charge}}" placeholder="5.00">
                                 @if ($errors->has('per_student_charge'))
                                     <span class="help-block">
                                     <strong>{{ $errors->first('per_student_charge') }}</strong>
@@ -80,36 +80,36 @@
                                 <label for="invoice_generation_date" class="control-label false-padding-bottom">Invoice Generation Date<label class="text-danger">*</label></label>
                                 <select id="invoice_generation_date" class="form-control select2" name="invoice_generation_date" >
                                     <option disabled selected>Select Date</option>
-                                    <option value="1" @if(old('invoice_generation_date')== 1) selected @endif>1</option>
-                                    <option value="2" @if(old('invoice_generation_date')== 2) selected @endif>2</option>
-                                    <option value="3" @if(old('invoice_generation_date')== 3) selected @endif>3</option>
-                                    <option value="4" @if(old('invoice_generation_date')== 4) selected @endif>4</option>
-                                    <option value="5" @if(old('invoice_generation_date')== 5) selected @endif>5</option>
-                                    <option value="6" @if(old('invoice_generation_date')== 6) selected @endif>6</option>
-                                    <option value="7" @if(old('invoice_generation_date')== 7) selected @endif>7</option>
-                                    <option value="8" @if(old('invoice_generation_date')== 8) selected @endif>8</option>
-                                    <option value="9" @if(old('invoice_generation_date')== 9) selected @endif>9</option>
-                                    <option value="10" @if(old('invoice_generation_date')== 10) selected @endif>10</option>
-                                    <option value="11" @if(old('invoice_generation_date')== 11) selected @endif>11</option>
-                                    <option value="12" @if(old('invoice_generation_date')== 12) selected @endif>12</option>
-                                    <option value="13" @if(old('invoice_generation_date')== 13) selected @endif>13</option>
-                                    <option value="14" @if(old('invoice_generation_date')== 14) selected @endif>14</option>
-                                    <option value="15" @if(old('invoice_generation_date')== 15) selected @endif>15</option>
-                                    <option value="16" @if(old('invoice_generation_date')== 16) selected @endif>16</option>
-                                    <option value="17" @if(old('invoice_generation_date')== 17) selected @endif>17</option>
-                                    <option value="18" @if(old('invoice_generation_date')== 18) selected @endif>18</option>
-                                    <option value="19" @if(old('invoice_generation_date')== 19) selected @endif>19</option>
-                                    <option value="20" @if(old('invoice_generation_date')== 20) selected @endif>20</option>
-                                    <option value="21" @if(old('invoice_generation_date')== 21) selected @endif>21</option>
-                                    <option value="22" @if(old('invoice_generation_date')== 22) selected @endif>22</option>
-                                    <option value="23" @if(old('invoice_generation_date')== 23) selected @endif>23</option>
-                                    <option value="24" @if(old('invoice_generation_date')== 24) selected @endif>24</option>
-                                    <option value="25" @if(old('invoice_generation_date')== 25) selected @endif>25</option>
-                                    <option value="26" @if(old('invoice_generation_date')== 26) selected @endif>26</option>
-                                    <option value="27" @if(old('invoice_generation_date')== 27) selected @endif>27</option>
-                                    <option value="28" @if(old('invoice_generation_date')== 28) selected @endif>28</option>
-                                    <option value="29" @if(old('invoice_generation_date')== 29) selected @endif>29</option>
-                                    <option value="30" @if(old('invoice_generation_date')== 30) selected @endif>30</option>
+                                    <option value="1" @if($schoolMeta->invoice_generation_date == 1) selected @endif>1</option>
+                                    <option value="2" @if($schoolMeta->invoice_generation_date == 2) selected @endif>2</option>
+                                    <option value="3" @if($schoolMeta->invoice_generation_date == 3) selected @endif>3</option>
+                                    <option value="4" @if($schoolMeta->invoice_generation_date == 4) selected @endif>4</option>
+                                    <option value="5" @if($schoolMeta->invoice_generation_date == 5) selected @endif>5</option>
+                                    <option value="6" @if($schoolMeta->invoice_generation_date == 6) selected @endif>6</option>
+                                    <option value="7" @if($schoolMeta->invoice_generation_date == 7) selected @endif>7</option>
+                                    <option value="8" @if($schoolMeta->invoice_generation_date == 8) selected @endif>8</option>
+                                    <option value="9" @if($schoolMeta->invoice_generation_date == 9) selected @endif>9</option>
+                                    <option value="10" @if($schoolMeta->invoice_generation_date == 10) selected @endif>10</option>
+                                    <option value="11" @if($schoolMeta->invoice_generation_date == 11) selected @endif>11</option>
+                                    <option value="12" @if($schoolMeta->invoice_generation_date == 12) selected @endif>12</option>
+                                    <option value="13" @if($schoolMeta->invoice_generation_date == 13) selected @endif>13</option>
+                                    <option value="14" @if($schoolMeta->invoice_generation_date == 14) selected @endif>14</option>
+                                    <option value="15" @if($schoolMeta->invoice_generation_date == 15) selected @endif>15</option>
+                                    <option value="16" @if($schoolMeta->invoice_generation_date == 16) selected @endif>16</option>
+                                    <option value="17" @if($schoolMeta->invoice_generation_date == 17) selected @endif>17</option>
+                                    <option value="18" @if($schoolMeta->invoice_generation_date == 18) selected @endif>18</option>
+                                    <option value="19" @if($schoolMeta->invoice_generation_date == 19) selected @endif>19</option>
+                                    <option value="20" @if($schoolMeta->invoice_generation_date == 20) selected @endif>20</option>
+                                    <option value="21" @if($schoolMeta->invoice_generation_date == 21) selected @endif>21</option>
+                                    <option value="22" @if($schoolMeta->invoice_generation_date == 22) selected @endif>22</option>
+                                    <option value="23" @if($schoolMeta->invoice_generation_date == 23) selected @endif>23</option>
+                                    <option value="24" @if($schoolMeta->invoice_generation_date == 24) selected @endif>24</option>
+                                    <option value="25" @if($schoolMeta->invoice_generation_date == 25) selected @endif>25</option>
+                                    <option value="26" @if($schoolMeta->invoice_generation_date == 26) selected @endif>26</option>
+                                    <option value="27" @if($schoolMeta->invoice_generation_date == 27) selected @endif>27</option>
+                                    <option value="28" @if($schoolMeta->invoice_generation_date == 28) selected @endif>28</option>
+                                    <option value="29" @if($schoolMeta->invoice_generation_date == 29) selected @endif>29</option>
+                                    <option value="30" @if($schoolMeta->invoice_generation_date == 30) selected @endif>30</option>
                                 </select>
                                 @if ($errors->has('invoice_generation_date'))
                                     <span class="help-block">
@@ -123,7 +123,7 @@
                         <div class="false-padding-bottom-form form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <div class="col-md-12">
                                 <label for="email" class="control-label false-padding-bottom">Email<label class="text-danger">*</label></label>
-                                <input id="email" type="text" class="form-control" name="email" value="{{old('email')}}" placeholder="Email">
+                                <input id="email" type="text" class="form-control" name="email" value="{{$schoolMeta->email}}" placeholder="Email">
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                     <strong>{{ $errors->first('email') }}</strong>
