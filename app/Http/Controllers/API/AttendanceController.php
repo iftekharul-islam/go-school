59434f86a43cf51f9911446211ce9bc52ae1a772
Debashish Roy <debashish@augnitive.com>
2020-02-06 17:47:34 +0600

    junk code removed

--- app/Http/Controllers/API/AttendanceController.php
+++ app/Http/Controllers/API/AttendanceController.php
@@ -17,26 +17,6 @@ use App\StuffAttendance;
 class AttendanceController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
