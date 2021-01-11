37053017c6c4efc7271442abd7eace90b1920abe
Iftekhar <iftekharul@augnitive.com>
2021-01-11 16:13:56 +0600

    Model relation name change

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -22,9 +22,11 @@ class ExamController extends Controller
 {
     protected $examService;
 
-    public function __construct(ExamService $examService){
+    public function __construct(ExamService $examService)
+    {
         $this->examService = $examService;
     }
+
     /**
      * Display a listing of the resource.
      *
@@ -33,21 +35,26 @@ class ExamController extends Controller
     public function index()
     {
         $exams = $this->examService->getLatestExamsBySchoolIdWithPagination();
-        return view('exams.all',compact('exams'));
+
+        return view('exams.all', compact('exams'));
     }
 
-    public function indexActive(){
-        $exams = $this->examService->getActiveExamsBySchoolId();
+    public function indexActive()
+    {
+        $exams                      = $this->examService->getActiveExamsBySchoolId();
         $this->examService->examIds = $exams->pluck('id')->toArray();
-        $courses = $this->examService->getCoursesByExamIds();
-        return view('exams.active',compact('exams','courses'));
+        $courses                    = $this->examService->getCoursesByExamIds();
+
+        return view('exams.active', compact('exams', 'courses'));
     }
 
-    public function details($exam_id) {
-        $exams = $this->examService->getActiveExamsBySchoolId();
+    public function details($exam_id)
+    {
+        $exams                      = $this->examService->getActiveExamsBySchoolId();
         $this->examService->examIds = $exams->pluck('id')->toArray();
-        $courses = $this->examService->getCoursesByExamIds();
-        return view('exams.details',compact('exam_id','courses'));
+        $courses                    = $this->examService->getCoursesByExamIds();
+
+        return view('exams.details', compact('exam_id', 'courses'));
     }
 
     /**
@@ -57,25 +64,27 @@ class ExamController extends Controller
      */
     public function create()
     {
-        $classes = $this->examService->getClassesBySchoolId();
+        $classes                  = $this->examService->getClassesBySchoolId();
         $already_assigned_classes = $this->examService->getAlreadyAssignedClasses();
-        $exams = $this->examService->getLatestExamsBySchoolIdWithPagination();
-        return view('exams.add',compact('classes','already_assigned_classes', 'exams'));
+        $exams                    = $this->examService->getLatestExamsBySchoolIdWithPagination();
+
+        return view('exams.add', compact('classes', 'already_assigned_classes', 'exams'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param   \Illuminate\Http\Request  $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(CreateExamRequest $request)
     {
         $this->examService->request = $request;
-        try{
+        try {
             $this->examService->storeExam();
-        } catch (\Exception $e){
-            return 'Error: '. $e->getMessage();
+        } catch (\Exception $e) {
+            return 'Error: '.$e->getMessage();
         }
 
         return redirect()->route('exams')->with('status', 'New Exam Created');
@@ -84,7 +93,8 @@ class ExamController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param   int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -95,24 +105,27 @@ class ExamController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param   int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        $exam = Exam::with('myClasses', 'myClasses.classDetails')->findOrFail($id);
-        $classes = $this->examService->getClassesBySchoolId();
+        $exam                     = Exam::with('myClasses', 'myClasses.classDetails')->findOrFail($id);
+        $classes                  = $this->examService->getClassesBySchoolId();
         $already_assigned_classes = $this->examService->getAlreadyAssignedClasses();
-        $exams = $this->examService->getLatestExamsBySchoolIdWithPagination();
-        return view('exams.edit',compact('classes','already_assigned_classes', 'exams', 'exam'));
+        $exams                    = $this->examService->getLatestExamsBySchoolIdWithPagination();
+
+        return view('exams.edit', compact('classes', 'already_assigned_classes', 'exams', 'exam'));
     }
 
-    public function updateExam(UpdateExamDetailsRequest $request, $id) {
-        $exam = Exam::findOrFail($id);
-        $exam->term = !empty($request->get('other_term')) ? $request->get('other_term') : $request->get('term');
-        $exam->exam_name = $request->get('exam_name');
+    public function updateExam(UpdateExamDetailsRequest $request, $id)
+    {
+        $exam             = Exam::findOrFail($id);
+        $exam->term       = !empty($request->get('other_term')) ? $request->get('other_term') : $request->get('term');
+        $exam->exam_name  = $request->get('exam_name');
         $exam->start_date = $request->get('start_date');
-        $exam->end_date = $request->get('end_date');
+        $exam->end_date   = $request->get('end_date');
         $exam->save();
 
         if (isset($request->classes)) {
@@ -122,24 +135,26 @@ class ExamController extends Controller
 
             foreach ($request->classes as $item) {
 
-                ExamForClass::where('exam_id', $id)
-                    ->where('class_id', $item)
-                    ->updateOrInsert([
-                        'exam_id' => $id,
-                        'class_id' => $item,
-                        'active' => 1,
-                    ]);
+                $data = ExamForClass::firstOrCreate([
+                    'exam_id'  => $id,
+                    'class_id' => $item,
+                ]);
+                $data->active = 1;
+                $data->save();
+
             }
 
         }
+
         return redirect()->route('exams')->with('status', 'Exam record updated');
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param   \Illuminate\Http\Request  $request
+     * @param   int                       $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
@@ -147,19 +162,21 @@ class ExamController extends Controller
         $request->validate([
             'exam_id' => 'required|numeric',
         ]);
-        try{
+        try {
             $this->examService->request = $request;
             $this->examService->updateExam();
-        } catch (\Exception $e){
-            return 'Error: '. $e->getMessage();
+        } catch (\Exception $e) {
+            return 'Error: '.$e->getMessage();
         }
+
         return back()->with('status', 'Exam record updated');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param   int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
@@ -167,7 +184,7 @@ class ExamController extends Controller
         $exam = Exam::findOrFail($id);
         $exam->delete();
         $examForClass = ExamForClass::where('exam_id', $id)->get();
-        $courses = Course::where('exam_id', $id)->get();
+        $courses      = Course::where('exam_id', $id)->get();
         foreach ($courses as $course) {
             $course->exam_id = 0;
             $course->save();
@@ -175,20 +192,22 @@ class ExamController extends Controller
         foreach ($examForClass as $class) {
             $class->delete();
         }
+
         return redirect()->back()->with('status', 'Exam Deleted');
     }
 
     /**update exam result file*/
-    public function updateResultFile(UploadExamResultRequest $request, $exam_id) {
+    public function updateResultFile(UploadExamResultRequest $request, $exam_id)
+    {
         $exam = Exam::findOrFail($exam_id);
 
         if (!empty($exam->result_file)) {
             Storage::disk('public')->delete($exam->result_file);
         }
 
-        $date = Carbon::now();
-        $fileName = Auth::user()->school_id.'_'.$date->format('Y_m_d').'_'.$date->format('g_i_a');
-        $path = Storage::disk('public')->put('result', $request->file('result_file'));
+        $date              = Carbon::now();
+        $fileName          = Auth::user()->school_id.'_'.$date->format('Y_m_d').'_'.$date->format('g_i_a');
+        $path              = Storage::disk('public')->put('result', $request->file('result_file'));
         $exam->result_file = $path;
         $exam->save();
 
@@ -196,24 +215,32 @@ class ExamController extends Controller
     }
 
     /**edit exam result file*/
-    public function editResultFile($exam_id) {
+    public function editResultFile($exam_id)
+    {
         $exam = Exam::findOrFail($exam_id);
+
         return view('exams.result.upload-result', compact('exam'));
     }
 
     /**upload exam result file*/
-    public function resultFiles() {
+    public function resultFiles()
+    {
         $exams = $exams = $this->examService->getLatestExamsBySchoolIdWithPagination();
+
         return view('exams.result.results', compact('exams'));
     }
+
     /**download result */
-    public function downloadResultFile($exam_id) {
+    public function downloadResultFile($exam_id)
+    {
         $exam = Exam::findOrFail($exam_id);
+
         return Storage::disk('public')->download($exam->result_file);
     }
 
     /**remove exam result file*/
-    public function removeResultFile($exam_id) {
+    public function removeResultFile($exam_id)
+    {
         $exam = Exam::findOrFail($exam_id);
 
         if (!empty($exam->result_file)) {
@@ -227,63 +254,71 @@ class ExamController extends Controller
     }
 
     /**Add exam attendees
-     * 
-    * @param int $exam_id
-    * @return \Illuminate\Http\Response
-    */
-    public function addAttendee(Request $request, $exam_id) {
-        $exam = Exam::findOrFail($exam_id);
+     *
+     * @param   int  $exam_id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function addAttendee(Request $request, $exam_id)
+    {
+        $exam   = Exam::findOrFail($exam_id);
         $search = $request->class_id ? $request->class_id : '';
-        
+
         $relatedClassIds = ExamForClass::where('exam_id', $exam->id)->groupBy('class_id')->pluck('class_id')->toArray();
-        $relatedClasses = Myclass::whereIn('id', $relatedClassIds)->get();
-       
+        $relatedClasses  = Myclass::whereIn('id', $relatedClassIds)->get();
+
         $students = User::join('sections', 'sections.id', 'users.section_id')
             ->join('classes', 'sections.class_id', 'classes.id')
             ->whereIn('sections.class_id', $relatedClassIds)
             ->where('users.role', 'student')
-            ->when($exam->attendees, function($query) use ($exam){
+            ->when($exam->attendees, function ($query) use ($exam) {
                 $query->whereNotIn('users.id', unserialize($exam->attendees));
             })
-            ->select('users.id','users.name', 'users.student_code', 'sections.section_number', 'sections.id as section_id', 'classes.class_number')
+            ->select('users.id', 'users.name', 'users.student_code', 'sections.section_number',
+                'sections.id as section_id', 'classes.class_number')
             ->paginate(30);
-        
-        return view('exams.attendee.addAttendees', compact('exam', 'students','relatedClasses', 'search'));
+
+        return view('exams.attendee.addAttendees', compact('exam', 'students', 'relatedClasses', 'search'));
     }
 
     /**Attendees
-     * 
-    * @param int $exam_id
-    * @return \Illuminate\Http\Response
-    */
-    public function attendees($exam_id) {
-        $exam = Exam::findOrFail($exam_id);
+     *
+     * @param   int  $exam_id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function attendees($exam_id)
+    {
+        $exam     = Exam::findOrFail($exam_id);
         $students = '';
-        if($exam->attendees) { 
+        if ($exam->attendees) {
             $students = User::with('section.class')->whereIn('id', unserialize($exam->attendees))->paginate(30);
         }
-       
+
         return view('exams.attendee.attendees', compact('exam', 'students'));
     }
+
     /**Store Attendees
-     * 
-    * @param int $exam_id
-    * @return \Illuminate\Http\Response
-    */
-    public function storeAttendees(Request $request, $exam_id) {
+     *
+     * @param   int  $exam_id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function storeAttendees(Request $request, $exam_id)
+    {
         $exam = Exam::findOrFail($exam_id);
-        
+
         if (empty($request->user_ids)) {
             return back()->with('status', 'No Student Selected');
         }
-        
-        $oldAttendees = !empty($exam->attendees) ? unserialize($exam->attendees) : [];
-        $newAttendees =  $request->user_ids;
-        $newAttendees =  array_merge($newAttendees, $oldAttendees);
-        $newAttendees = array_unique($newAttendees);
+
+        $oldAttendees    = !empty($exam->attendees) ? unserialize($exam->attendees) : [];
+        $newAttendees    = $request->user_ids;
+        $newAttendees    = array_merge($newAttendees, $oldAttendees);
+        $newAttendees    = array_unique($newAttendees);
         $exam->attendees = serialize($newAttendees);
         $exam->save();
-        
+
         return back()->with('status', 'Attendee(s) Added');
     }
 }
