8850fbadd8d57b722878c8e988dc7f1e34d9c6c5
Debashish Roy <debashish@augnitive.com>
2022-06-02 13:07:39 +0600

    Create Teacher testing

--- resources/views/email/user/welcome.blade.php
+++ resources/views/email/user/welcome.blade.php
@@ -2,25 +2,25 @@
 
     # Welcome to {{ config('app.name') }}
 
-    Hi {{ $name }},
+{{--    Hi {{ $name }},--}}
 
-    We are glad to have you on board.
+{{--    We are glad to have you on board.--}}
 
-    @if(!is_null($password))
-        Your login details are as follows:
+{{--    @if(!is_null($password))--}}
+{{--        Your login details are as follows:--}}
 
-        **Email**: {{ $email }}
+{{--        **Email**: {{ $email }}--}}
 
-        **Password**: {{ $password }}
+{{--        **Password**: {{ $password }}--}}
 
-        You can change your password once logged-in.
-    @else
-        Please ask site administrator to know your login access.
-    @endif
+{{--        You can change your password once logged-in.--}}
+{{--    @else--}}
+{{--        Please ask site administrator to know your login access.--}}
+{{--    @endif--}}
 
-    @component('vendor.mail.markdown.button', ['url' => url('login')])
-        Visit site
-    @endcomponent
+{{--    @component('vendor.mail.markdown.button', ['url' => url('login')])--}}
+{{--        Visit site--}}
+{{--    @endcomponent--}}
 
     Thanks,
     {{ config('app.name') }}
